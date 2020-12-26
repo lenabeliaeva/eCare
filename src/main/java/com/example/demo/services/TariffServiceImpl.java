ecf71afe068f26a3422879ec4b817879416148d0
lenabeliaeva <lena.belia@yandex.ru>
2020-12-26 16:10:54 +0300

    Traisactional annotation is added

--- src/main/java/com/example/demo/services/TariffServiceImpl.java
+++ src/main/java/com/example/demo/services/TariffServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.demo.dao.TariffDaoImpl;
 import com.example.demo.models.Tariff;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -13,21 +14,25 @@ public class TariffServiceImpl implements TariffService {
     private final TariffDao tariffDao = new TariffDaoImpl();
 
     @Override
+    @Transactional
     public void add(Tariff tariff) {
         tariffDao.add(tariff);
     }
 
     @Override
+    @Transactional
     public List<Tariff> getAll() {
         return tariffDao.getAll();
     }
 
     @Override
+    @Transactional
     public void delete(Tariff tariff) {
         tariffDao.delete(tariff);
     }
 
     @Override
+    @Transactional
     public Tariff getById(long id) {
         return tariffDao.getById(id);
     }
