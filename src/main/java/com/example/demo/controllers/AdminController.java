e0bf1ae1b7d7ea8cb6135552f286add8858c7e66
lenabeliaeva <lena.belia@yandex.ru>
2021-01-15 00:36:56 +0300

    GetMapping instead of RequestMapping

--- src/main/java/com/example/demo/controllers/AdminController.java
+++ src/main/java/com/example/demo/controllers/AdminController.java
@@ -1,13 +1,12 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    @GetMapping(value = "/admin")
     public String showAdminPage() {
         return "admin";
     }
