1b8f79c16c685c7d187cd0d88a216ee4d54bfdb8
lenabeliaeva <lena.belia@yandex.ru>
2021-01-20 12:32:40 +0300

    I wanted to add PersistenceContext annotation but nothing worked

--- src/main/java/com/example/demo/dao/OptionDaoImpl.java
+++ src/main/java/com/example/demo/dao/OptionDaoImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
