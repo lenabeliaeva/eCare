eb1adbcb739d18929d8669d72ef0a532854f994e
lenabeliaeva <lena.belia@yandex.ru>
2021-01-20 12:30:48 +0300

    Contract dao layer is realised

--- src/main/java/com/example/demo/dao/ContractDao.java
+++ src/main/java/com/example/demo/dao/ContractDao.java
@@ -1,6 +1,13 @@
 package com.example.demo.dao;
 
-import com.example.demo.models.Client;
+import com.example.demo.models.Contract;
+
+import java.util.List;
 
 public interface ContractDao {
+    Contract getById(long id);
+    List<Contract> getByClientId(long clientId);
+    void save(Contract contract);
+    void update(Contract contract);
+    void delete (Contract contract);
 }
--- src/main/java/com/example/demo/dao/ContractDaoImpl.java
+++ src/main/java/com/example/demo/dao/ContractDaoImpl.java
@@ -1,12 +1,16 @@
 package com.example.demo.dao;
 
 import com.example.demo.models.Client;
+import com.example.demo.models.Contract;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import java.util.List;
 
+@Repository
 public class ContractDaoImpl implements ContractDao {
 
     private static final EntityManagerFactory emf;
@@ -16,4 +20,39 @@ public class ContractDaoImpl implements ContractDao {
     }
 
     private final EntityManager em = emf.createEntityManager();
+
+    @Override
+    public Contract getById(long id) {
+        return em.find(Contract.class, id);
+    }
+
+    @Override
+    public List<Contract> getByClientId(long clientId) {
+        return em
+                .createQuery("select c from Contract c where c.client.id = :cId")
+                .setParameter("cId", clientId)
+                .getResultList();
+    }
+
+    @Override
+    public void save(Contract contract) {
+        em.getTransaction().begin();
+        em.persist(contract);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public void update(Contract contract) {
+        em.getTransaction().begin();
+        em.merge(contract);
+        em.getTransaction().commit();
+
+    }
+
+    @Override
+    public void delete(Contract contract) {
+        em.getTransaction().begin();
+        em.remove(contract);
+        em.getTransaction().commit();
+    }
 }
