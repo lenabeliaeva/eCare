a80abcc8ca4f27e6f0f45a577670a9662ac0f9e4
lenabeliaeva <lena.belia@yandex.ru>
2021-02-22 14:02:06 +0300

    Removed unused imports

--- src/main/java/com/example/demo/services/OptionServiceImpl.java
+++ src/main/java/com/example/demo/services/OptionServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.RollbackException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
